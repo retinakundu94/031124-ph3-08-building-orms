c7519d4753463330da0c6d9c16ffc05dd8747764
brewchetta <chett.tiller@gmail.com>
2023-04-10 16:17:49 -0400

    LOTS OF SQL AND STUFF

--- lib/movie.py
+++ lib/movie.py
@@ -2,13 +2,24 @@ from . import CONN
 from . import CURSOR
 
 class Movie:
-    def __init__(self, title, year):
-        self.id = None
+    def __init__(self, title, year, id=None):
+        self.id = id
         self.title = title
-        self.year = year
+        self._year = year
 
     def __repr__(self):
         return f'<Movie id={self.id} title={self.title} year={self.year}>'
+
+    def get_year(self):
+        return self._year
+
+    def set_year(self, year):
+        if year >= 2000 and isinstance(year, int):
+            self._year = year
+        else:
+            print("That movie is too old and doesn't matter")
+
+    year = property(get_year, set_year)
 
     # OBJECT RELATIONAL MAPPING / ORM
 
@@ -21,3 +32,45 @@ class Movie:
         )
         """
         CURSOR.execute(sql)
+
+    def save(self):
+        if self.id:
+            self.update()
+        else:
+            self.create()
+
+    def create(self):
+        sql = """INSERT INTO movies (title, year)
+        VALUES (?, ?)
+        """
+        CURSOR.execute(sql, [self.title, self.year])
+        CONN.commit()
+        self.id = CURSOR.execute("SELECT * FROM movies ORDER BY id DESC LIMIT 1").fetchone()[0]
+
+    def update(self):
+        sql = "UPDATE movies SET title = ?, year = ? WHERE id = ?"
+        CURSOR.execute(sql, (self.title, self.year, self.id) )
+        CONN.commit()
+
+    @classmethod
+    def fetch_all(cls):
+        sql = "SELECT * FROM movies"
+        all_movies = CURSOR.execute(sql).fetchall()
+        movies_list = []
+        for movie_data in all_movies:
+            movie = Movie(movie_data[1], movie_data[2])
+            movie.id = movie_data[0]
+            movies_list.append(movie)
+
+        return movies_list
+
+    @classmethod
+    def destroy(cls, id):
+        sql = "DELETE FROM movies WHERE id = ?"
+        CURSOR.execute(sql, (id,))
+        CONN.commit()
+
+    def destroy(self):
+        sql = "DELETE FROM movies WHERE id = ?"
+        CURSOR.execute(sql, (self.id,))
+        CONN.commit()
