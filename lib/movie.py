7a61cc2f9e5b89ac3c17143bd3e6cc650bf746d3
brewchetta <chett.tiller@gmail.com>
2023-04-10 14:24:52 -0400

    able to create tables

--- lib/movie.py
+++ lib/movie.py
@@ -2,10 +2,22 @@ from . import CONN
 from . import CURSOR
 
 class Movie:
-    def __init__(self, title, runtime):
+    def __init__(self, title, year):
         self.id = None
         self.title = title
-        self.runtime = runtime
+        self.year = year
 
     def __repr__(self):
-        return f'<Movie id={self.id} title={self.title} runtime={self.runtime}>'
+        return f'<Movie id={self.id} title={self.title} year={self.year}>'
+
+    # OBJECT RELATIONAL MAPPING / ORM
+
+    @classmethod
+    def create_table(cls):
+        sql = """CREATE TABLE IF NOT EXISTS movies (
+        id INTEGER PRIMARY KEY,
+        title TEXT,
+        year INTEGER
+        )
+        """
+        CURSOR.execute(sql)
