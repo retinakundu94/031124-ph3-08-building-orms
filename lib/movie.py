0e3b999c620cb57dcb09768a1825b8a2d8a96c2d
brewchetta <chett.tiller@gmail.com>
2023-08-28 15:10:37 -0400

    movie power

--- lib/movie.py
+++ lib/movie.py
@@ -25,14 +25,33 @@ class Movie:
         return CURSOR.execute(sql)
         # CONN.commit()
 
+
     @classmethod
     def get_all(cls):
-        pass
+        sql="SELECT * FROM movies"
 
-    # BONUS: this will destroy a row based on a given id
+        movie_tuples = CURSOR.execute(sql).fetchall()
+        return [ Movie(title=movie[1], year=movie[2], id=movie[0]) for movie in movie_tuples ]
+
+
     @classmethod
     def destroy_by_id(cls, id):
-        pass
+        sql = "DELETE FROM movies WHERE id = ?"
+
+        CURSOR.execute(sql, [ id ])
+        CONN.commit()
+
+
+    @classmethod
+    def get_by_id(cls, id):
+        sql="""SELECT * FROM movies
+        WHERE id = ?
+        """
+
+        tuple = CURSOR.execute(sql, [id]).fetchone()
+
+        if tuple:
+            return Movie(id=tuple[0], title=tuple[1], year=tuple[2])
 
 
     # --- SQL INSTANCE METHODS --- #
@@ -52,19 +71,27 @@ class Movie:
         self.id = movie_tuple[0]
 
 
-
-
-
-
-
-
-
     def update(self):
-        pass
+        sql="""UPDATE movies
+        SET title = ?, year = ?
+        WHERE id = ?
+        """
 
-    # BONUS: this will either create or update depending on if this instance exists in the db
+        CURSOR.execute(sql, [ self.title, self.year, self.id ])
+        CONN.commit()
+
+
     def save(self):
-        pass
+        if self.id:
+            self.update()
+        else:
+            self.create()
 
+    
     def destroy(self):
-        pass
+        sql="""DELETE FROM movies
+        WHERE id = ?
+        """
+
+        CURSOR.execute(sql, [self.id])
+        CONN.commit()
