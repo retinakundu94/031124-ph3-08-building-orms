85a65e71c7cac7b47455a8274398dd9127f6862b
Retina Kundu <retinakundu94@gmail.com>
2024-04-29 12:57:49 -0400

    add changes

--- lib/practice.py
+++ lib/practice.py
@@ -15,38 +15,77 @@ class Movie:
 
     @classmethod
     def create_table(cls):
-        pass
-        # creates the table if it doesn't exist
+        sql='''CREATE TABLE IF NOT EXISTS movies (
+            id INTEGER PRIMARY KEY,
+            title TEXT,
+            year INTEGER
+        )'''
+
+        CURSOR.execute(sql)
+        CONN.commit()
 
     @classmethod
     def get_all(cls):
-        pass
-        # creates a new instance for each row in the db
+        sql='''SELECT * FROM movies'''
+
+        read_all_tuples = CURSOR.execute(sql).fetchall()
+
+        all_movies = []
+
+        for tup in read_all_tuples:
+            Movie = Movie(name=tup[1], id=tup[0])
+            all_movies.append(movies)
+
+        return all_movies
 
     @classmethod
     def get_by_id(cls, id:int):
-        pass
+        sql = ' ' ' SELECT * FROM courses WHERE id = ? ' ' '
+
+        found_movie_tuple = CURSOR.execute(sql, [ id ]).fetchone()
+
+        if found_movie_tuple:
+            return Movie(name=found_movie_tuple[1], id=found_movie_tuple[0])
+        
         # finds by id and if found instantiates a new instance
 
 
     # --- SQL INSTANCE METHODS --- #
 
     def create(self):
-        pass
-        # creates in the db and updates instance with the new id
+        sql='''INSERT INTO movies (title, year)
+        VALUES ( ? , ?)
+        '''
 
+        CURSOR.execute(sql, [self.title, self.year])
+        CONN.commit()
+
+        last_row_sql = 'SELECT * FROM movies ORDER BY id DESC LIMIT 1'
+        last_row_tuple = CURSOR.execute(last_row_sql).fetchone()
+
+        self.id = last_row_tuple[0]
 
     def update(self):
-        pass
-        # updates the row based on current attributes 
+        sql = '''UPDATE movies SET title = ? , year = ?
+        WHERE id = ?
+        '''
+        CURSOR.execute(sql, [self.title, self.year, self.id])
+        CONN.commit ()
 
 
     def save(self):
-        pass
-        # creates if it doesn't exist
+       if not self.id:
+           self.create() #creates if it doesn't exist
+       else:
+           self.update()
         # updates if it does exist
 
     
     def destroy(self):
-        pass
+        sql='''DELETE FROM courses WHERE id = ?'''
+
+        CURSOR.execute(sql, [self.id])
+        CONN.commit()
+
+        self.id = None
         # deletes the instance from the db and removes the id
